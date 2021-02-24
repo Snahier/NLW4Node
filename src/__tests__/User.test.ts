a2c3f99e150326f00b8c9d5fb6086d4e3228d05f
Snahier <hansalmeida@hotmail.com>
2021-02-24 11:29:12 -0300

    Configure test request + database setup

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,15 @@
+import request from "supertest"
+import { app } from "../app"
+import createConnection from "../database"
+
+describe("User", () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
+
+  request(app).post("/users").send({
+    email: "user@example.com",
+    name: "User Example",
+  })
+})
