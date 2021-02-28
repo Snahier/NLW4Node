a08e174cbe3a24c8988958b46d21c037ff6cd3cf
Snahier <hansalmeida@hotmail.com>
2021-02-27 22:05:16 -0300

    Add send email service

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm"
 import { SurveysRepository } from "../repositories/SurveysRepository"
 import { SurveysUsersRepository } from "../repositories/SurveyUsersRepository"
 import { UsersRepository } from "../repositories/UsersRepository"
+import SendMailService from "../services/SendMailService"
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -20,9 +21,9 @@ class SendMailController {
       })
     }
 
-    const surveyExists = await surveysRepository.findOne({ id: survey_id })
+    const survey = await surveysRepository.findOne({ id: survey_id })
 
-    if (!surveyExists) {
+    if (!survey) {
       return response.status(400).json({
         error: "Survey does not exist",
       })
@@ -33,6 +34,8 @@ class SendMailController {
       survey_id,
     })
     await surveysUsersRepository.save(surveyUser)
+
+    await SendMailService.execute(email, survey.title, survey.description)
 
     return response.json(surveyUser)
   }
