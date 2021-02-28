2502f39e688488971f02ae6b5d53180af07cc9e2
Snahier <hansalmeida@hotmail.com>
2021-02-27 22:26:05 -0300

    Move responsibilities from service to controller

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import path from "path"
 import { getCustomRepository } from "typeorm"
 import { SurveysRepository } from "../repositories/SurveysRepository"
 import { SurveysUsersRepository } from "../repositories/SurveyUsersRepository"
@@ -13,9 +14,9 @@ class SendMailController {
     const surveysRepository = getCustomRepository(SurveysRepository)
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
 
-    const userExists = await usersRepository.findOne({ email })
+    const user = await usersRepository.findOne({ email })
 
-    if (!userExists) {
+    if (!user) {
       return response.status(400).json({
         error: "User does not exist",
       })
@@ -30,12 +31,26 @@ class SendMailController {
     }
 
     const surveyUser = surveysUsersRepository.create({
-      user_id: userExists.id,
+      user_id: user.id,
       survey_id,
     })
     await surveysUsersRepository.save(surveyUser)
 
-    await SendMailService.execute(email, survey.title, survey.description)
+    const npsPath = path.resolve(
+      __dirname,
+      "..",
+      "views",
+      "emails",
+      "npsMail.hbs"
+    )
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+    }
+
+    await SendMailService.execute(email, survey.title, variables, npsPath)
 
     return response.json(surveyUser)
   }
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,7 +1,6 @@
 import fs from "fs"
 import handlebars from "handlebars"
 import nodemailer, { Transporter } from "nodemailer"
-import path from "path"
 
 class SendMailService {
   private client: Transporter
@@ -22,22 +21,11 @@ class SendMailService {
     })
   }
 
-  async execute(to: string, subject: string, body: string) {
-    const npsPath = path.resolve(
-      __dirname,
-      "..",
-      "views",
-      "emails",
-      "npsMail.hbs"
-    )
-    const templateFileContent = fs.readFileSync(npsPath).toString("utf-8")
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8")
 
     const mailTemplateParse = handlebars.compile(templateFileContent)
-    const html = mailTemplateParse({
-      name: to,
-      title: subject,
-      description: body,
-    })
+    const html = mailTemplateParse(variables)
 
     const message = await this.client.sendMail({
       to,
